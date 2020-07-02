3a2f617042d11dbc0b7b42957852694c8853a9d1
Andrew W. Harn <andrew.harn@broadcom.com>
2020-07-02 10:43:11 -0400

    rearrange the code
    Signed-off-by: Andrew W. Harn <andrew.harn@broadcom.com>

--- index.js
+++ index.js
@@ -3,17 +3,35 @@ const github = require('@actions/github');
 const fs = require('fs');
 const request = require('request');
 
+function callback(error, response, body) {
+  bodyString = error;
+  bodyString = bodyString + " Body: " + body;
+  bodyString = bodyString + " Response: " + response;
+  json = JSON.parse(body);
+
+  for (object in json) {
+    files.push(object.filename);
+    if (object.filename.contains(file)) {
+      changed = true;
+      var contents = fs.readFileSync(object.filename);
+      if (contents.contains(header)) {
+        headerFound = true;
+      }
+    }
+  }
+}
+
 try {
   const header = core.getInput('header');
   const file = core.getInput('file');
   const token = core.getInput('token');
   const reqUrl = `https://api.github.com/repos/${github.context.payload.repository.full_name}}/pulls/${github.context.payload.pull_request.number}/files`;
   
-  let changed = false;
-  let headerFound = false;
-  let json;
-  let bodyString;
-  let files = [];
+  var changed = false;
+  var headerFound = false;
+  var json;
+  var bodyString;
+  var files = [];
 
   const reqOptions = {
     url: reqUrl,
@@ -24,23 +42,7 @@ try {
     }
   };
 
-  request(reqOptions, function(err, res, body) {
-    bodyString = err;
-    bodyString = bodyString + " Body: " + body;
-    bodyString = bodyString + " Res: " + res;
-    json = JSON.parse(body);
-
-    for (object in json) {
-      files.push(object.filename);
-      if (object.filename.contains(file)) {
-        changed = true;
-        var contents = fs.readFileSync(object.filename);
-        if (contents.contains(header)) {
-          headerFound = true;
-        }
-      }
-    }
-  });  
+  request(reqOptions, callback);
 
   console.log(`Files: ${files}; Changelog found: ${changed}; Header found: ${headerFound}; Json: ${JSON.stringify(json)}; Body: ${bodyString}`);
 
