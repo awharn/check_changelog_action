e180188bcf46a4dc77fde75ee72e8d6def720bf4
zFernand0 <37381190+zFernand0@users.noreply.github.com>
2024-02-05 13:20:14 +0000

    test: temporary lerna support
    Signed-off-by: zFernand0 <37381190+zFernand0@users.noreply.github.com>

--- index.js
+++ index.js
@@ -83,7 +83,18 @@ async function checkChangelog() {
         core.error(`Only Yarn 1.x and 2.x are supported. ${REPORT_ISSUE}`);
       }
     } else if (lerna) {
+      let isLernaTemp = false;
+      if (!fs.existsSync("lerna.json")) {
+        isLernaTemp = true;
+        fs.writeFileSync("lerna.json", JSON.stringify({
+            version: packageJson.version,
+            useWorkspaces: true
+        }, null, 2));
+      }
       packages = JSON.parse(await execAndReturnOutput(`${npxCmd} lerna@6 list --since origin/${baseRef} --exclude-dependents --json --loglevel silent`));
+      if (isLernaTemp) {
+        fs.rmSync("lerna.json", {force: true});
+      }
     }
 
     for (const package of packages) {
